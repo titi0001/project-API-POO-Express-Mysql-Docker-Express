959c67b66ccd70f50429e946cca30cfaacea957c
Thiago Pereira <titiura@gmail.com>
2023-02-19 11:26:36 -0300

    feat: req06- ok validateProduct

---
+++ src/middlewares/validateproduct.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+const validateFieldsProduct = Joi.object().keys({
+  name: Joi.string().min(3).required(),
+  amount: Joi.string().min(3).required(),
+});
+
+export default async function ValidateProduct(req: Request, res: Response, next: NextFunction) {
+  const { error } = await validateFieldsProduct.validate(req.body);
+
+  console.log(error?.details[0].type);
+  
+  if (error?.details[0].type === 'string.base' || error?.details[0].type === 'string.min') {
+    return res.status(422).send({ message: error.details[0].message });
+  }
+
+  if (error !== undefined) {
+    return res.status(400).send({ message: error.details[0].message });
+  }
+
+  next();
+}
--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import ProductController from '../controllers/product.controller';
+import ValidateProduct from '../middlewares/validateproduct';
 
 const productController = new ProductController();
 const router = Router();
 
 router.get('/', (req, res) => productController.findAllProduct(req, res));
-router.post('/', (req, res) => productController.createProduct(req, res));
+router.post('/', ValidateProduct, (req, res) => productController.createProduct(req, res));
 
 export default router; 
